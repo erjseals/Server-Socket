2f112ad2063adb2b18514099d4ed97e199bae690
Eric <ericseals@ku.edu>
2019-08-16 14:54:13 -0500

    Cleaned up the code, wrapped in try/catch

--- src/com/example/TCPServer/TCPServer.java
+++ src/com/example/TCPServer/TCPServer.java
@@ -11,11 +11,13 @@ public class TCPServer {
     public static void main(String[] args) throws Exception {
 
         //initialize socket and input stream
-        Socket		 socket = null;
-        Socket       socket2 = null;
+        Socket socket = null;
         ServerSocket server = null;
-        DataInputStream in	 = null;
-        OutputStream out = null;
+        DataInputStream in = null;
+        ByteArrayInputStream bis = null;
+        BufferedImage bImage = null;
+        DataOutputStream dos = null;
+        ByteArrayOutputStream bos = null;
 
         //socket ip and port
         String phoneAddress = "192.168.108.118";
@@ -29,100 +31,116 @@ public class TCPServer {
 
         int length;
         byte[] data;
-        while(true) {
+        while (true) {
             try {
-                server = new ServerSocket(portNum);
-                System.out.println("Server started");
+                //Try to open the socket
+                try {
+                    server = new ServerSocket(portNum);
 
-                System.out.println("Waiting for a client ...");
+                    System.out.println("Server started");
+                    System.out.println("Waiting for a client ...");
 
-                socket = server.accept();
-
-                System.out.println("Client accepted");
-
-                // takes input from the client socket
-                in = new DataInputStream(socket.getInputStream());
-
-                // Receiving data from client
-                length = in.readInt();
-                System.out.println("Length of Array: " + length);
-
-                data = new byte[length];
-
-                if (length > 0) {
-                    in.readFully(data, 0, data.length);
+                    socket = server.accept();
+                    System.out.println("Client accepted");
+                } catch (Exception e) {
+                    System.out.println("Error accepting Client: " + e);
+                    System.out.println("Line number: " + e.getStackTrace()[0].getLineNumber());
                 }
 
-                ByteArrayInputStream bis = new ByteArrayInputStream(data);
-                BufferedImage bImage = ImageIO.read(bis);
-                ImageIO.write(bImage, "jpg", new File(receivedImagePath));
-                System.out.println("Image created!");
-
-                ProcessBuilder processBuilder = new ProcessBuilder(darknetArgArr[0], darknetArgArr[1], darknetArgArr[2], darknetArgArr[3], darknetArgArr[4]);
-                processBuilder.directory(new File(darknetPath));
-
-                System.out.println("Processing Image!");
-
+                //Socket is connected, try to get the array
                 try {
+                    // takes input from the client socket
+                    in = new DataInputStream(socket.getInputStream());
+
+                    // Receiving data from client
+                    length = in.readInt();
+                    System.out.println("Length of Array: " + length);
+
+                    data = new byte[length];
+
+                    if (length > 0) {
+                        in.readFully(data, 0, data.length);
+                    }
+
+                    bis = new ByteArrayInputStream(data);
+                    bImage = ImageIO.read(bis);
+                    ImageIO.write(bImage, "jpg", new File(receivedImagePath));
+
+                    System.out.println("Image created!");
+
+                    in.close();
+                    socket.close();
+
+                } catch (Exception e) {
+                    System.out.println("Error getting byteArray and creating file: " + e);
+                    System.out.println("Line number: " + e.getStackTrace()[0].getLineNumber());
+                }
+
+                //try to call darknet for processing
+                try {
+                    ProcessBuilder processBuilder = new ProcessBuilder(darknetArgArr[0], darknetArgArr[1], darknetArgArr[2], darknetArgArr[3], darknetArgArr[4]);
+                    processBuilder.directory(new File(darknetPath));
+
+                    System.out.println("Processing Image!");
                     Process process = processBuilder.start();
                     int errCode = process.waitFor();
                     System.out.println("Echo command executed, any errors? " + (errCode == 0 ? "No" : "Yes"));
                 } catch (Exception e) {
-                    System.out.println("in processBuilder.start()");
-                    System.out.println(e);
+                    System.out.println("Error calling darknet to process: " + e);
+                    System.out.println("Line number: " + e.getStackTrace()[0].getLineNumber());
                 }
 
-                in.close();
-                socket.close();
-
-                File tempFile = new File(processedImagePath);
-                boolean exists = tempFile.exists();
-                System.out.println("File exists: " + exists);
-
-                //Now we try to send back!
-                socket2 = new Socket(phoneAddress, portNum);
-                if (socket.isConnected()) {
-                    System.out.println("Socket is connected!");
+                try {
+                    File tempFile = new File(processedImagePath);
+                    boolean exists = tempFile.exists();
+                    System.out.println("File exists: " + exists);
+                } catch (Exception e) {
+                    System.out.println("Error such that darknet did not create the image: " + e);
+                    System.out.println("Line number: " + e.getStackTrace()[0].getLineNumber());
                 }
 
-                DataOutputStream dos = new DataOutputStream(socket2.getOutputStream());
+                try {
+                    //Now we try to send back!
+                    socket = new Socket(phoneAddress, portNum);
+                    if (socket.isConnected()) {
+                        System.out.println("Socket is connected!");
+                    }
 
-                //Need to get the image and extract a bytearray
+                    dos = new DataOutputStream(socket.getOutputStream());
 
-                bImage = ImageIO.read(new File(processedImagePath));
+                    //Need to get the image and extract a bytearray
 
-                System.out.println("Test 1");
+                    bImage = ImageIO.read(new File(processedImagePath));
+                    bos = new ByteArrayOutputStream();
+                    ImageIO.write(bImage, "jpg", bos);
+                    byte[] data2 = bos.toByteArray();
+                    length = data2.length;
 
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    System.out.println("Length of Array: " + length);
 
-                System.out.println("Test 2");
+                    dos.writeInt(length);
+                    dos.write(data2, 0, length);
+                } catch (Exception e) {
+                    System.out.println("Error in sending the new image back: " + e);
+                    System.out.println("Line number: " + e.getStackTrace()[0].getLineNumber());
+                }
 
-                ImageIO.write(bImage, "jpg", bos);
-
-                System.out.println("Test 3");
-
-                byte[] data2 = bos.toByteArray();
-
-                System.out.println("Test 4");
-
-                length = data2.length;
-
-                System.out.println("Length of Array: " + length);
-
-                dos.writeInt(length);
-                dos.write(data2, 0, length);
-
-                System.out.println("Test 5");
-
-                socket.getOutputStream().close();
-                dos.close();
-                socket.close();
-
-
+                try {
+                    socket.getOutputStream().close();
+                    dos.close();
+                    socket.close();
+                    server.close();
+                    in.close();
+                    bis.close();
+                    bos.close();
+                } catch (Exception e) {
+                    System.out.println("Error closing everything: " + e);
+                    System.out.println("Line number: " + e.getStackTrace()[0].getLineNumber());
+                }
 
             } catch (Exception e) {
-                System.out.println("general error!");
-                System.out.println(e);
+                System.out.println("general error!" + e);
+                System.out.println("Line number: " + e.getStackTrace()[0].getLineNumber());
             }
         }
     }
